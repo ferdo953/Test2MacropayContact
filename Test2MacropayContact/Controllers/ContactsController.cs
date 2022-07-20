721f5d9664a8a07dbaa5c3254e3e69a6616e6b43
Fernando <fernandoespinosa19x@gmail.com>
2022-07-20 01:25:57 -0500

    Agregar archivos de proyecto.

---
+++ Test2MacropayContact/Controllers/ContactsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Web;
+using System.Web.Mvc;
+using Test2MacropayContact.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Test2MacropayContact.Controllers
+{
+    public class ContactsController : DefaultController
+    {
+
+        // GET: api/<ContactsController>
+        //https://localhost:44377/api/Contacts?phrase=Az
+        [Microsoft.AspNetCore.Mvc.HttpGet()]
+        public ActionResult<List<Contacts>> Get()
+        {
+            Request.ContentType = "application/json";
+            var request = HttpContext.Request.Query["phrase"];
+            if(request.ToString() == null || request.ToString().Length == 0)
+            {
+                return BadRequest();
+            }
+            string FileJsoN = System.IO.File.ReadAllText(@"C:\Users\sysadmin\source\repos\Test2MacropayContact\Test2MacropayContact\Database\fakedatabase.js");
+            var res = JsonSerializer.Deserialize<List<Contacts>>(FileJsoN).AsQueryable().OrderBy(x => x.name).ToList();
+            var resultado = res.FindAll(x => x.name.ToUpper().Contains(request.ToString().ToUpper()));
+            return resultado;
+        }
+
+        // GET api/<ContactsController>/cf63ff0d-ecaa-448d-9456-a225a44c3159
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]
+        public Microsoft.AspNetCore.Mvc.ActionResult<Contacts> Get(string id)
+        {
+            string FileJsoN = System.IO.File.ReadAllText(@"C:\Users\sysadmin\source\repos\Test2MacropayContact\Test2MacropayContact\Database\fakedatabase.js");
+            var res = JsonSerializer.Deserialize<List<Contacts>>(FileJsoN);
+            var resultado = res.Find(x => x.id == id);
+            if(resultado != null)
+            {
+                return resultado;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE api/<ContactsController>/cf63ff0d-ecaa-448d-9456-a225a44c3159
+        [Microsoft.AspNetCore.Mvc.HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+             string FileJsoN = System.IO.File.ReadAllText(@"C:\Users\sysadmin\source\repos\Test2MacropayContact\Test2MacropayContact\Database\fakedatabase.js");
+             var res = JsonSerializer.Deserialize<List<Contacts>>(FileJsoN);
+            foreach (var item in res)
+            {
+                if (item.id == id) 
+                {
+                   var validacion = res.Remove(item);
+                    System.IO.TextWriter writeFile = new StreamWriter(@"C:\Users\sysadmin\source\repos\Test2MacropayContact\Test2MacropayContact\Database\fakedatabase.js");
+                    writeFile.Write(JsonSerializer.Serialize(res));
+                    writeFile.Flush();
+                    writeFile.Close();
+                    if (validacion)
+                    {
+                        return NoContent();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Test2MacropayContact/Models/Contacts.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Test2MacropayContact.Models
+{
+    public class Contacts
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public List<string> addressLines { get; set; }
+    }
+
+    public class Adress
+    {
+        public int Id { get; set; }
+    }
+}
